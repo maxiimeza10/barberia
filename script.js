24be644fba279d3095ae126e012f7fe68f24fdd0
maximiliano <109862193+maxiimeza10@users.noreply.github.com>
2024-05-16 20:30:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+
+// boton click del menu navbar
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('nav');
+
+    menuToggle.addEventListener('click', function () {
+        nav.classList.toggle('active');
+    });
+});
